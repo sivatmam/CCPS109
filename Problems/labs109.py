97e7b5d258ecf604d6a877cd82fdbb3e83d91296
Sivatma Maharaj <sivatma.maharaj@gmail.com>
2020-04-07 13:24:02 -0400

    Problem 4 only_odd_digits

--- Problems/labs109.py
+++ Problems/labs109.py
@@ -139,6 +139,32 @@ def riffle(items, out = True):
       riffled_items.append(first_half[index])
 
   return riffled_items     
-  
 
+
+"""
+Only odd digits 
+ 
+def only_odd_digits(n): 
+
+Check that the given positive integer​ n contains only odd digits (1, 3, 5, 7 and 9)
+when it is written out. Return ​True if this is the case, and ​False otherwise. Note that this 
+question is not asking whether the number ​n itself is odd or even. You therefore will have to 
+look at every digit of the given number before you can claim that the number contains no odd digits. 
+ 
+Hint: to extract the lowest digit of a positive integer ​n​, use the expression​n % 10​. To extract all                    other digits except the lowest one, use the expression ​n // 10​. Or, if you don't want to be this                    fancy, first convert the number into a string and work there.  
+ 
+n Expected result 
+8 False 
+1357975313579 True 
+42 False 
+71358 False 
+0 False   
+"""
+
+def only_odd_digits(n):
+  str_n = str(n)
+  for index in str_n:
+    if int(index) % 2 == 0:
+      return False
+  return True
 
